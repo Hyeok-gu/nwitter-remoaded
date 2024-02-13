049f1443c816edb27ee2ce54ee2383bae798cb0b
Hyeok-gu <thanatos6737@naver.com>
2024-02-13 22:47:18 +0900

    트윗올리고 데이터 전송, 이미지도 전송

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,22 +1,137 @@
+import { addDoc, collection, updateDoc } from "firebase/firestore";
+import { useState } from "react";
 import styled from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 12px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+const AttachFileButton = styled.label`
+  padding: 10px 0;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+const AttachFileInput = styled.input`
+  display: none;
+`;
+const SubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`;
 
 export default function PostTweetForm() {
-  const Form = styled.form``;
-  const TextArea = styled.textarea``;
-  const AttachFileButton = styled.label`
-    cursor: pointer;
-  `;
-  const AttachFileInput = styled.input`
-    display: none;
-  `;
-  const SubmitBtn = styled.input``;
+  const [isLoading, setLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    const maxSize = 1 * 1024 * 1024; //* 1MB 사이즈 제한
+    if (files && files.length === 1) {
+      setFile(files[0]);
+      const fileSize = files[0].size;
+      console.log(fileSize);
+      if (fileSize > maxSize) {
+        alert("1MB 이상의 파일은 업로드할 수 없습니다.");
+        setFile(null);
+        return;
+      }
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+    try {
+      setLoading(true);
+      const doc = await addDoc(collection(db, "tweets"), {
+        //addDoc 함수는 Firestore에 문서를 추가하고 해당 문서의 참조를 반환, 따라서 doc 변수에는 추가된 문서의 정보가 들어 있다. 이 문서의 id 속성을 통해 해당 문서의 고유 식별자를 가져올 수 있다.
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, file); // promise를 반환하는데, 그 결과값에 업로드 결과에 대한 참조가 있다.
+        const url = await getDownloadURL(result.ref); // 업로드가 완료된 파일의 퍼블릭 경로를 string으로 가져온다.
+        await updateDoc(doc, { photo: url }); //참조할 값을 먼저 넣고 추가할 값을 넣는다. 이미 참조할 doc이 위에 있으므로 간단.
+      }
+      setFile(null);
+      setTweet("");
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <Form>
-      <TextArea placeholder="what is happening" />
-      <AttachFileButton htmlFor="file">Add Photo</AttachFileButton>
-      <AttachFileInput type="file" id="file" accept="image/*" />
-      <SubmitBtn />
+    <Form onSubmit={onSubmit}>
+      <TextArea
+        required
+        rows={5}
+        maxLength={180}
+        onChange={onChange}
+        placeholder="what is happening"
+        value={tweet}
+      />
+      <AttachFileButton htmlFor="file">
+        {file ? "Photo added ✅" : "Add Photo"}
+      </AttachFileButton>
+      <AttachFileInput
+        onChange={onFileChange}
+        type="file"
+        id="file"
+        accept="image/*"
+      />
+      <SubmitBtn
+        type="submit"
+        value={isLoading ? "Posting..." : "Post Tweet"}
+      />
     </Form>
   );
 }
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyAJICzv2X8cemW_tii0s_TZWissSvp4YiE",
   authDomain: "nwitter-reloaded-d3e7d.firebaseapp.com",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
